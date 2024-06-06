c259506369c79ca03f2a1dfc56934245cced3167
tryska <160110280+tryskas@users.noreply.github.com>
2024-06-06 14:59:10 +0200

    Add files via upload

---
+++ Unote/user_portal/views.py
@@ -0,0 +1,23 @@
+from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
+from administration.models import Subject, Grade, UE, Group
+from django.shortcuts import render
+
+
+class DashboardView(TemplateView):
+    template_name = "user_portal/dashboard.html"
+
+
+@login_required
+def studentview(request):
+    user = request.user
+    ues = UE.objects.all()
+    subjects = Subject.objects.all()
+    grades = Grade.objects.all()
+    user_promo = Group.objects.filter(type="promo", users=user).first()
+
+    context = {'user': user, 'subjects': subjects, 'grades': grades, 'ues':
+        ues,
+               'user_promo': user_promo}
+    return render(request, 'user_portal/studentview.html',
+                  context)
