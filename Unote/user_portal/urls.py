a87c83cd61d12f160df09758550541c53be2edfc
RemiRoutier <153221334+RemiRoutier@users.noreply.github.com>
2024-06-07 14:10:36 +0200

    Delete Unote directory

--- Unote/user_portal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "user_portal"
-urlpatterns = [
-    path("dashboard/", views.DashboardView.as_view(),
-         name="dashboard"),
-    path('notes/', views.studentview, name="notes")
-]
