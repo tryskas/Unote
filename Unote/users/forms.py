e1a0a18638f662bad5a2ca1bbd779daee5e93387
Simon <antropius.simon00@gmail.com>
2024-06-10 09:05:53 +0200

    Correction bug création utilisateur

--- Unote/users/forms.py
+++ Unote/users/forms.py
@@ -9,6 +9,25 @@ class CustomAuthenticationForm(AuthenticationForm):
         self.fields['username'].label = "Identifiant"
 
 
+def generate_unique_username():
+    batch_size = 1000
+    last_number = -1
+    query_set = CustomUser.objects.filter(
+        username__regex=r'^\d+$').values_list('username', flat=True)
+
+    for batch in range(0, query_set.count(), batch_size):
+        usernames = query_set[batch:batch + batch_size]
+        existing_numbers = sorted(int(username) for username in usernames)
+
+        for number in existing_numbers:
+            if number != last_number + 1:
+                break
+            last_number = number
+
+    new_number = last_number + 1
+    return str(new_number).zfill(8)
+
+
 class CustomUserCreationForm(ModelForm):
     class Meta:
         model = CustomUser
@@ -19,30 +38,12 @@ class CustomUserCreationForm(ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.username = self.generate_unique_username()
+        user.username = generate_unique_username()
         password = CustomUser.objects.make_random_password(length=15)
         user.set_password(password)
         if commit:
             user.save()
         return user, password
-
-    def generate_unique_username(self):
-        batch_size = 1000
-        last_number = 0
-        query_set = CustomUser.objects.filter(
-            username__regex=r'^\d+$').values_list('username', flat=True)
-
-        for batch in range(0, query_set.count(), batch_size):
-            usernames = query_set[batch:batch + batch_size]
-            existing_numbers = sorted(int(username) for username in usernames)
-
-            for number in existing_numbers:
-                if number != last_number + 1:
-                    break
-                last_number = number
-
-        new_number = last_number + 1
-        return str(new_number).zfill(8)
 
 
 class CSVUploadForm(Form):
